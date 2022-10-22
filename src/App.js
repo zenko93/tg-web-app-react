0fc5eb707fa1153c0df8042d7820adcfd387b33d
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2022-10-22 21:50:55 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header';
 
 function App() {
     const { tg, onToggleButton } = useTelegram();
@@ -11,6 +12,7 @@ function App() {
 
     return (
         <div className="App">
+            <Header />
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
