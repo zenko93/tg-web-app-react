058af69e7112e280eef6e643da9073212fd8a127
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2022-10-24 19:27:18 +0300

    send data from product list

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../hooks/useTelegram';
@@ -10,8 +10,32 @@ const getTotalPrice = (items) => {
 };
 
 const ProductList = ({ products }) => {
-    const { tg } = useTelegram();
+    const { tg, queryId } = useTelegram();
     const [addedItems, setAddedItems] = useState([]);
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId
+        };
+
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    }, []);
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData);
+
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
+    }, [onSendData]);
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find((item) => item.id === product.id);
