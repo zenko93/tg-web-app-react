a8480eeab9a931c0f622ade5148e74f1c5f474e1
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2022-10-24 13:04:47 +0300

    added Form, ProductList

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
-    const { tg, onToggleButton } = useTelegram();
+    const { tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
@@ -13,7 +16,16 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <button onClick={onToggleButton}>toggle</button>
+            <Routes>
+                <Route
+                    index
+                    element={<ProductList />}
+                />
+                <Route
+                    path={'/form'}
+                    element={<Form />}
+                />
+            </Routes>
         </div>
     );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegram } from '../../hooks/useTelegram';
+
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        });
+    }, [tg]);
+
+    useEffect(() => {
+        if (!country || !city) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, city, tg]);
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value);
+    };
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value);
+    };
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value);
+    };
+
+    return (
+        <div className={'form'}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Город'}
+                value={city}
+                onChange={onChangeCity}
+            />
+            <select
+                value={subject}
+                onChange={onChangeSubject}
+                className={'select'}
+            >
+                <option value={'legal'}>Физ. лицо</option>
+                <option value={'physical'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
